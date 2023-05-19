e5bf809643b6d434d834aa700bf20844df26cd8b
20106 <Mohamed-yasserr>
2023-05-19 21:15:40 +0300

    changed the color

--- lib/tabs/Ahadeth_tab.dart
+++ lib/tabs/Ahadeth_tab.dart
@@ -5,7 +5,7 @@ class AhadethTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.red,
+      color: Colors.blue,
 
     );
   }
